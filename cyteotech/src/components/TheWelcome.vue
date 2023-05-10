730a007978c463b049a4bbdd586ff201cc92f5af
Kenshin_Bryz <84550697+BryLim-AI@users.noreply.github.com>
2023-05-10 19:30:05 +0800

    first commit

---
+++ cyteotech/src/components/TheWelcome.vue
@@ -0,0 +1,32 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+
+<h1>Digi Tech Hub</h1>
+<h3>Advanced Technologies</h3>
+
+</template>
+
+<style scoped>
+
+h1{
+  font-size: 5rem;
+  color:antiquewhite;
+  font-family:fantasy;
+}
+
+h3{
+  color:antiquewhite;
+  font-family:fantasy;
+  font-size: 2rem;
+}
+
+
+</style>
---
+++ cyteotech/src/views/AboutView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="about">
+   
+    <h2>Contents</h2>
+    <div class="container">
+
+      <a href="" target="_blank">
+        <p class="contentEdit"></p>
+      </a>
+
+    </div>
+
+  </div>
+</template>
+
+<style>
+
+h2{
+  padding: 20px;
+  color:aliceblue;
+  font-family:fantasy;
+  font-size: 2rem;
+}
+
+.container {
+background: rgba( 255, 255, 255, 0.25 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 4px );
+-webkit-backdrop-filter: blur( 4px );
+border-radius: 10px;
+border: 1px solid rgba( 255, 255, 255, 0.18 );
+width:550px;
+height:470px;
+}
+
+
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    align-items: center;
+  }
+}
+</style>
